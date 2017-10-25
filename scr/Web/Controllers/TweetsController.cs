373401d95718902a71023cd7828c9118d74d310e
Javier Gonzalez <javiergonzaleztapia@gmail.com>
2017-10-25 13:08:53 +0100

    Mvc web with simple tweet CRUD

---
+++ scr/Web/Controllers/TweetsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TweetAdvisor.Services;
+using Web.Models;
+using TweetAdvisor.Core;
+
+namespace Web.Controllers
+{
+    public class TweetsController : Controller
+    {
+        private readonly TweetsContext _context;
+
+        public TweetsController(TweetsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tweets
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tweets.ToListAsync());
+        }
+
+        // GET: Tweets/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tweet = await _context.Tweets
+                .SingleOrDefaultAsync(m => m.TweetId == id);
+            if (tweet == null)
+            {
+                return NotFound();
+            }
+
+            return View(tweet);
+        }
+
+        // GET: Tweets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tweets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TweetId,UserId,Date,Category,Latitude,Longitude,TweetText")] Tweet tweet)
+        {
+            if (ModelState.IsValid)
+            {
+                tweet.TweetId = Guid.NewGuid();
+                _context.Add(tweet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tweet);
+        }
+
+        // GET: Tweets/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tweet = await _context.Tweets.SingleOrDefaultAsync(m => m.TweetId == id);
+            if (tweet == null)
+            {
+                return NotFound();
+            }
+            return View(tweet);
+        }
+
+        // POST: Tweets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("TweetId,UserId,Date,Category,Latitude,Longitude,TweetText")] Tweet tweet)
+        {
+            if (id != tweet.TweetId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tweet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TweetExists(tweet.TweetId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tweet);
+        }
+
+        // GET: Tweets/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tweet = await _context.Tweets
+                .SingleOrDefaultAsync(m => m.TweetId == id);
+            if (tweet == null)
+            {
+                return NotFound();
+            }
+
+            return View(tweet);
+        }
+
+        // POST: Tweets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var tweet = await _context.Tweets.SingleOrDefaultAsync(m => m.TweetId == id);
+            _context.Tweets.Remove(tweet);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TweetExists(Guid id)
+        {
+            return _context.Tweets.Any(e => e.TweetId == id);
+        }
+    }
+}
